cb25bf7b63f0a1d23098db5ead41e247e4564334
Piorosen <chacha@udon.party>
2023-04-21 13:53:15 +0000

    빌드 자동화 코드 작성함.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-scons Werror=0 arch=arm64-v8a neon=1 opencl=0 embed_kernels=1 extra_cxx_flags="-fPIC" -j$(nproc)
+scons Werror=0 arch=arm64-v8a neon=1 opencl=0 examples=0 embed_kernels=1 exceptions=0 extra_cxx_flags="-fPIC" -j$(nproc)
+
+sshpass -p "linaro" scp ./build/*.so linaro@192.168.0.229:/home/linaro/Desktop/chacha
